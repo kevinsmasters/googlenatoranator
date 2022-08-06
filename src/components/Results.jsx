cb691e138fcab9d3e5cc638982d00286b013d7ad
kevinsmasters <gmail@kevinmasters.com>
2022-08-06 17:14:54 -0400

    stubb out search result array

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
+import ReactPlayer from 'react-player'
 
+import { useResultContext } from '../contexts/ResultContextProvider';
+import { Loading } from './Loading';
 export const Results = () => {
-  return (
-    <div>Results</div>
-  )
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
+  const location = useLocation();
+
+  useEffect(() => {
+    getResults('/search/q=Your Mom&num=40')
+  }, [])
+
+  if(isLoading) return <Loading />;
+
+  switch (location.pathname) {
+    case '/search':
+      return (
+        <div className="flex flex-wrap jistify-between space-y-6 sm:px-56">
+          {/* {results?.results?.map(( { link, title }) => (
+
+          ))} */}
+        </div>
+      );
+    case '/images':
+      return 'IMAGES';
+    case '/news':
+      return 'NEWS';
+    case '/videos':
+      return 'VIDEOS';
+  
+    default:
+      return 'ERROR';
+  }
 }
